a1cd2a510b49db62362860f2f6bbaecc2a7d6a85
John Gomes <johnwillian254@gmail.com>
2024-12-29 19:58:11 +0000

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function downloadPDF() {
+    const content = document.getElementById('content'); // Seleciona o conteúdo para o PDF
+    const options = {
+      margin: 1, // Margens do PDF
+      filename: 'pagina.pdf', // Nome do arquivo gerado
+      image: { type: 'jpeg', quality: 0.98 }, // Qualidade das imagens no PDF
+      html2canvas: { scale: 2 }, // Resolução
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' } // Configurações do PDF
+    };
+    
+    html2pdf().set(options).from(content).save(); // Gera e baixa o PDF
+  }
+  
