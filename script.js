cd2a9cf563650edea98baa3aeb3ecd77390f6469
John Gomes <johnwillian254@gmail.com>
2024-12-30 20:13:51 +0000

    1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function downloadPDF() {
-    const content = document.getElementById('content'); // Seleciona o conteúdo para o PDF
+    const content = document.querySelector('.content'); // Seleciona o conteúdo para o PDF
     const options = {
-      margin: 1, // Margens do PDF
-      filename: 'pagina.pdf', // Nome do arquivo gerado
-      image: { type: 'jpeg', quality: 0.98 }, // Qualidade das imagens no PDF
-      html2canvas: { scale: 2 }, // Resolução
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' } // Configurações do PDF
+        margin: 1, // Margens do PDF
+        filename: 'pagina.pdf', // Nome do arquivo gerado
+        image: { type: 'jpeg', quality: 0.98 }, // Qualidade das imagens no PDF
+        html2canvas: { scale: 2 }, // Resolução
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' } // Configurações do PDF
     };
-    
+
     html2pdf().set(options).from(content).save(); // Gera e baixa o PDF
-  }
-  
+}
